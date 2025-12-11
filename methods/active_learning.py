aeeed4f83de6e2190b1e8633cf581ecf103d82eb
FarukTekinCENG <faruktekinmail@gmail.com>
2025-12-11 18:43:39 +0300

    update

--- methods/active_learning.py
+++ methods/active_learning.py
@@ -5,10 +5,11 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 import train
+from model import ModelPredictor
 
 from data_utils.database import database
 
-class UncertaintySampling:
+class ActiveLearning:
     hyper_params = {
         "N": 100,       # number of samples selected each iteration
         "I": 0.001,     # ...
@@ -17,6 +18,9 @@ class UncertaintySampling:
     
     @staticmethod
     def model_predict(max_samples=None):
+        # 
+        predictor = ModelPredictor("./fine_tuned_eurobert")
+
         # SELECT is_labelled false samples in pool
         batch_size = 1000
         offset = 0
@@ -47,29 +51,39 @@ class UncertaintySampling:
             total_processed += len(batch)
             page += 1
 
-            # make model predict
-            # load model, give input, take output
+            for x in batch:
+                # make model predict
+                # load model, give input, take output
+                single_result = predictor.predict(x[1])
+                print(single_result["predicted_class"])
+                print(single_result["confidence"])
+                 
+                # save model answer db
+                
+                # calc score
+                #uncertainty_score=calc_score()
+                uncertainty_score=1-single_result["confidence"]
 
-            # save model answer db
-            
-            # calc score
-            #uncertainty_score=calc_score()
-            # save score db
-            #save_score_db(uncertainty_score)
+                # save score db
+                #save_score_db(uncertainty_score)
 
     # @staticmethod
     # def calc_score():
     #     # ...
     #     # calc score
     #
+
     # @staticmethod
     # def select_samples_to_train(N=100):
     #     # ...
     #
+
     # @staticmethod
     # def prep_labels():
     #     # ...
+    #     # for real scenarios > at this stage: label unlabelled data
     #
+
     # @staticmethod
     # def train_iterate(samples, labels):
     #     train.pipeline(samples, labels)
@@ -103,5 +117,5 @@ class UncertaintySampling:
         # print("success: ... rounds of training: ... max labels: ...")
 
 if __name__ == '__main__':
-    UncertaintySampling.run(2850)
+    ActiveLearning.run(5)
 
